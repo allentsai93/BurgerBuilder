5d9e13f866145dcd230935a0bde6d5e681cb514e
Allen Tsai <allentsai93@gmail.com>
2018-04-29 16:25:27 -0700

    added lazy loading for routes

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -3,10 +3,6 @@ import Aux from '../../../hoc/Aux/Aux';
 import Button from '../../UI/Button/Button';
 
 class OrderSummary extends Component {
-    // Could be a functional component
-    componentWillUpdate(){
-        console.log('[OrderSummary] Will Update')
-    }
 
     render() { 
         const ingredientSummary = Object.keys(this.props.ingredients)
